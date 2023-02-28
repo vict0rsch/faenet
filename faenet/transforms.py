80b25ac725199052c4b0e123e5ed05fd7ce90131
Victor Schmidt <vsch@pm.me>
2023-02-28 18:38:43 -0500

    update transforms baseline

--- faenet/transforms.py
+++ faenet/transforms.py
@@ -6,8 +6,108 @@ import torch
 import torch_geometric
 from torch_geometric.transforms import LinearTransformation
 
+from faenet.frame_averaging import (
+    frame_averaging_2D,
+    frame_averaging_3D,
+    data_augmentation,
+)
 
-class RandomRotate(object):
+from faenet.graph_rewiring import (
+    remove_tag0_nodes,
+    one_supernode_per_graph,
+    one_supernode_per_atom_type,
+    one_supernode_per_atom_type_dist,
+)
+
+
+class Transform:
+    def __call__(self, data):
+        raise NotImplementedError
+
+    def __str__(self):
+        name = self.__class__.__name__
+        items = [
+            f"{k}={v}"
+            for k, v in self.__dict__.items()
+            if not callable(v) and k != "inactive"
+        ]
+        s = f"{name}({', '.join(items)})"
+        if self.inactive:
+            s = f"[inactive] {s}"
+        return s
+
+
+class FrameAveraging(Transform):
+    def __init__(self, fa_type=None, fa_frames=None):
+        self.fa_frames = (
+            "random" if (fa_frames is None or fa_frames == "") else fa_frames
+        )
+        self.fa_type = "" if fa_type is None else fa_type
+        self.inactive = not self.fa_type
+        assert self.fa_type in {
+            "",
+            "2D",
+            "3D",
+            "DA",
+        }
+        assert self.fa_frames in {
+            "",  # equivalent to random, necessary still for sweeps
+            "random",
+            "det",
+            "all",
+            "multiple",
+            "se3-random",
+            "se3-det",
+            "se3-all",
+            "se3-multiple",
+        }
+
+        if self.fa_type:
+            if self.fa_type == "2D":
+                self.fa_func = frame_averaging_2D
+            elif self.fa_type == "3D":
+                self.fa_func = frame_averaging_3D
+            elif self.fa_type == "DA":
+                self.fa_func = data_augmentation
+            else:
+                raise ValueError(f"Unknown frame averaging: {self.fa_type}")
+
+    def __call__(self, data):
+        if self.inactive:
+            return data
+        return self.fa_func(data, self.fa_frames)
+
+
+class GraphRewiring(Transform):
+    def __init__(self, rewiring_type=None) -> None:
+        self.rewiring_type = rewiring_type
+
+        self.inactive = not self.rewiring_type
+
+        if self.rewiring_type:
+            if self.rewiring_type == "remove-tag-0":
+                self.rewiring_func = remove_tag0_nodes
+            elif self.rewiring_type == "one-supernode-per-graph":
+                self.rewiring_func = one_supernode_per_graph
+            elif self.rewiring_type == "one-supernode-per-atom-type":
+                self.rewiring_func = one_supernode_per_atom_type
+            elif self.rewiring_type == "one-supernode-per-atom-type-dist":
+                self.rewiring_func = one_supernode_per_atom_type_dist
+            else:
+                raise ValueError(f"Unknown self.graph_rewiring {self.graph_rewiring}")
+
+    def __call__(self, data):
+        if self.inactive:
+            return data
+
+        data.batch = torch.zeros(data.num_nodes, dtype=torch.long)
+        data.natoms = torch.tensor([data.natoms])
+        data.ptr = torch.tensor([0, data.natoms])
+
+        return self.rewiring_func(data)
+
+
+class RandomRotate(Transform):
     r"""Rotates node positions around a specific axis by a randomly sampled
     factor within a given interval.
 
@@ -77,7 +177,7 @@ class RandomRotate(object):
         )
 
 
-class RandomReflect(object):
+class RandomReflect(Transform):
     r"""Reflect node positions around a specific axis (x, y, x=y) or the origin
     z-axis remains fixed.
 
